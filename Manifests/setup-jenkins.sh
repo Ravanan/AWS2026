2b54b38acd9dfc12f51c94a623c7473d7f4f4a46
Ravanan <mailravan@gmail.com>
2026-04-20 17:38:25 +0530

    Update setup-jenkins.sh

--- Manifests/setup-jenkins.sh
+++ Manifests/setup-jenkins.sh
@@ -1,8 +1,14 @@
 
 #!/bin/bash
 
-# Usage: ./deploy-jenkins.sh <CLUSTER_NAME> <REGION> <NAMESPACE>
-# Example: ./deploy-jenkins.sh my-cluster us-east-1 jenkins
+# Usage: ./setup-jenkins.sh deploy|delete
+# Example: ./setup-jenkins.sh deploy
+
+#$0 → the script name (setup-jenkins.sh)
+#$1 → the first argument (deploy)
+
+ACTION=$1
+NAMESPACE="jenkins"
 
 if ! command -v helm &> /dev/null; then
     echo "Helm not found. Please install Helm before running this script."
@@ -10,35 +16,37 @@ if ! command -v helm &> /dev/null; then
 fi
 
 # Step 1: Connecting to EKS cluster
-NAMESPACE="jenkins"
-CLUSTER_NAME=$(kubectl config current-context)
-
-echo "Connecting to EKS cluster: $CLUSTER_NAME"
+if [ "$ACTION" == "deploy" ]; then
+echo "Connecting to EKS cluster: $(kubectl config current-context)"
 
 # Ensure namespace exists
 kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
 
+# Create PVC inline
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: jenkins-pvc
+  namespace: $NAMESPACE
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 10Gi
+  storageClassName: gp2
+EOF
 
-# Step 2: Verify cluster connection
-kubectl get nodes
 
 # Step 3: Add Jenkins Helm repo
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
 
-# Step 5: Deploy Jenkins
-helm install jenkins jenkinsci/jenkins --namespace $NAMESPACE \
-  --set persistence.enabled=true \
-  --set persistence.existingClaim=jenkins-pvc \
-  --set controller.startupProbe.httpGet.path=/ \
-  --set controller.startupProbe.httpGet.port=8080 \
-  --set controller.startupProbe.initialDelaySeconds=480 \
-  --set controller.startupProbe.periodSeconds=30 \
-  --set controller.startupProbe.failureThreshold=20
-
-echo "waiting for startup probe to initiate after 480 seconds for Jenkins: $CLUSTER_NAME"
-
-# Step 6: Wait for Jenkins pod
+# Deploy Jenkins using values.yaml
+helm install jenkins jenkinsci/jenkins -n $NAMESPACE -f values.yaml
+    
+echo "Waiting for Jenkins pod to start..."
 kubectl get pods -n $NAMESPACE
 
 # Step 7: Get Jenkins admin password
@@ -46,3 +54,10 @@ kubectl get secret jenkins -n $NAMESPACE -o jsonpath="{.data.jenkins-admin-passw
 
 # Step 8: Port forward Jenkins service
 kubectl port-forward svc/jenkins 8080:8080 -n $NAMESPACE
+
+elif [ "$ACTION" == "delete" ]; then
+    helm uninstall jenkins -n $NAMESPACE
+    kubectl delete namespace $NAMESPACE
+else
+    echo "Usage: $0 {deploy|delete}"
+fi
